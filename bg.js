9484ae75e45a4b1bac0b802dc6b4fa79fc55e2a4
taehopark0! <taehojj@gmail.com>
2020-12-18 01:16:48 +0900

    To-do, Background, Geography

---
+++ bg.js
@@ -0,0 +1,24 @@
+const body = document.querySelector("body");
+
+const IMG_NUMBER = 9;
+
+
+function paintImage(imgNumber) {
+    const image = new Image();
+    image.src = `images/${imgNumber + 1}.jpeg`;
+    image.classList.add("bgImage");
+    body.prepend(image);
+
+}
+
+function genRandom() {
+    const number = Math.floor(Math.random() * 9);
+    return number;
+}
+
+function init() {
+    const randomNumber = genRandom();
+    paintImage(randomNumber);
+}
+
+init();
